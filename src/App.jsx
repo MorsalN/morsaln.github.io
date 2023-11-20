33aada1ed5dbb385f229a32f47d8674385aaccc9
Morsal Niyaz <morsalniyaz@gmail.com>
2023-11-20 15:03:30 -0800

    Adding all components for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 // Can create a component quickly using rafce Enter
 
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Exeperience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Exeperience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
