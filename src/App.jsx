5f0f6e52534e5b71db7e64d70a11f8d009144669
Morsal Niyaz <morsalniyaz@gmail.com>
2023-11-20 14:30:41 -0800

    App component setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Can create a component quickly using rafce Enter
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// Rendering the App component App.jsx inside the div #root
+ReactDOM.render(<App />, document.getElementById('root'));
