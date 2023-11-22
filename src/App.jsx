b89d2219bc88596a20849eaa0c6a7a8cec141145
Morsal Niyaz <morsalniyaz@gmail.com>
2023-11-22 14:53:55 -0800

    Created sections and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 // Can create a component quickly using rafce Enter
 
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Exeperience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Exeperience from "./components/experience/Experience";
+// import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+// import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -19,13 +18,13 @@ const App = () => {
       <Nav />
       <About />
       <Exeperience />
-      <Services />
+      {/* <Services /> */}
       <Portfolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import "./nav.css";
+import { IoHomeOutline } from "react-icons/io5";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { RiBook2Line } from "react-icons/ri";
+import { BiMessageDetail } from "react-icons/bi";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href="#" className="active">
+        <IoHomeOutline />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <MdOutlineWorkOutline />
+      </a>
+      <a href="#portfolio">
+        <RiBook2Line />
+      </a>
+      <a href="#contact">
+        <BiMessageDetail />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
