00ab27c152053085d37a55380c1059a684c25603
Morsal Niyaz <morsalniyaz@gmail.com>
2023-11-22 16:36:40 -0800

    Making the nav bar responsive

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,23 +5,49 @@ import { AiOutlineUser } from "react-icons/ai";
 import { MdOutlineWorkOutline } from "react-icons/md";
 import { RiBook2Line } from "react-icons/ri";
 import { BiMessageDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : " "}
+      >
         <IoHomeOutline />
       </a>
-      <a href="#about">
+
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : " "}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : " "}
+      >
         <MdOutlineWorkOutline />
       </a>
-      <a href="#portfolio">
+
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "portfolio" ? "active" : " "}
+      >
         <RiBook2Line />
       </a>
-      <a href="#contact">
+
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : " "}
+      >
         <BiMessageDetail />
       </a>
     </nav>
