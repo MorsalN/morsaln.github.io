a8554ec60448d234da078f566e1c8d02407a7122
Morsal Niyaz <morsalniyaz@gmail.com>
2023-11-27 14:31:23 -0800

    About section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,10 @@ import "./header.css";
 import CTA from "./CTA";
 import Me from "./../../assets/me-gif4.gif";
 import HeaderSocials from "./HeaderSocials";
+import { useState } from "react";
 
 const Header = () => {
+  const [activeScroll, setActiveScroll] = useState("#");
   return (
     <header>
       <div className="container header__container">
@@ -18,7 +20,13 @@ const Header = () => {
           <img src={Me} alt="" className="img__me" />
         </div>
 
-        <a href="#contact" className="scroll__down">
+        <a
+          href="#contact"
+          onClick={() => setActiveScroll("#contact")}
+          className={
+            activeScroll === "#contact" ? "active scroll__down" : "scroll__down"
+          }
+        >
           Scroll Down
         </a>
       </div>
